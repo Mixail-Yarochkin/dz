3cbb37d3e404eee064c92ccb6c529ddfbdf5966e
Mixail-Yarochkin <120408169+Mixail-Yarochkin@users.noreply.github.com>
2022-12-12 20:39:59 +0300

    Add files via upload

---
+++ dzciklspar.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n, k;
+    cout << "N:";
+    cin >> n;
+    cout << "K:";
+    cin >> k;
+    int i, i2;
+    float rez = 0, nk;
+    for (i = 1; i <= n; ++i) {
+        nk = 1;
+        for (i2 = 1; i2 <= k; ++i2) nk *= i;
+        rez += nk;
+    }
+    cout << rez;
+    return 0;
+}
