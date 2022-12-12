332b2b416075499c02e3ae438842c48712210bd7
Mixail-Yarochkin <120408169+Mixail-Yarochkin@users.noreply.github.com>
2022-12-12 20:42:04 +0300

    Add files via upload

---
+++ dzlogvr.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "A:";
+	cin >> a;
+	cout << "B:";
+	cin >> b;
+	cout << "C:";
+	cin >> c;
+	if ((a + b) > c && (a + c) > b && (b + c) > a) {
+		cout << "Истинно";
+	}
+	else {
+		cout << "Ложь";
+	}
+}
