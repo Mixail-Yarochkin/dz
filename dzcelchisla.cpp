bcb6e8601aed866de06239bca05fc17893f56935
Mixail-Yarochkin <120408169+Mixail-Yarochkin@users.noreply.github.com>
2022-12-12 20:55:09 +0300

    Add files via upload

---
+++ dzcelchisla.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    int  proz;
+    cin >> a;
+    c = a / 10;
+    b = a % 10;
+    proz = c * b;
+    cout << "Произведение чисел:" << proz;
+}
